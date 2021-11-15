5630a2690d0e92dd7cb1d6e1b386ecc3dcd165da
Yakushin103 <vyakushin103@yandex.ru>
2021-11-15 12:58:43 +0300

    Added theme in project

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import NextLink from 'next/link'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
@@ -10,13 +12,39 @@ import Link from '@mui/material/Link'
 import { useStyles } from '../utils/styles'
 
 export default function Layout({ title, description, children }) {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0'
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0'
+      },
+      body1: {
+        fontWeight: 'normal',
+      }
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000'
+      },
+      secondary: {
+        main: '#208080'
+      }
+    }
+  })
   const classes = useStyles()
 
   return (
     <div>
       <Head>
         <title>
-          {title ? `${title} - Next Amazona` : "Next Amazona"} 
+          {title ? `${title} - Next Amazona` : "Next Amazona"}
         </title>
         {
           description &&
@@ -24,39 +52,42 @@ export default function Layout({ title, description, children }) {
         }
       </Head>
 
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>amazona</Typography>
-            </Link>
-          </NextLink>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
+            </NextLink>
 
-          <div className={classes.grow}></div>
+            <div className={classes.grow}></div>
 
-          <NextLink href="/cart" passHref>
-            <Link>
-              Cart
-            </Link>
-          </NextLink>
+            <NextLink href="/cart" passHref>
+              <Link>
+                Cart
+              </Link>
+            </NextLink>
 
-          <NextLink href="/login" passHref>
-            <Link>
-              Login
-            </Link>
-          </NextLink>
-        </Toolbar>
-      </AppBar>
+            <NextLink href="/login" passHref>
+              <Link>
+                Login
+              </Link>
+            </NextLink>
+          </Toolbar>
+        </AppBar>
 
-      <Container className={classes.main}>
-        {children}
-      </Container>
+        <Container className={classes.main}>
+          {children}
+        </Container>
 
-      <footer className={classes.footer}>
-        <Typography>
-          All rights reserved. Next Amazona.
-        </Typography>
-      </footer>
+        <footer className={classes.footer}>
+          <Typography>
+            All rights reserved. Next Amazona.
+          </Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   )
 }
